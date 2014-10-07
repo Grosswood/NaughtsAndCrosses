acb12c0013afaa6baf35a16e11837dd8cab80898
Grosswood <mozge4ok@gmail.com>
2014-10-07 15:21:04 +0600

    AI++, but check "return combination;" lane
    Signed-off-by: Grosswood <mozge4ok@gmail.com>

--- WindowsForms/Form1.cs
+++ WindowsForms/Form1.cs
@@ -18,69 +18,78 @@ namespace WindowsForms
             InitializeComponent();
         }
 
-        public void AITrun()
+        public void AITurn()
         {
             Random random = new Random();
-            int boxNumber = 9;
-            while (emptyBox[boxNumber] > 1 && emptyBox[9] < 10)
+            int boxNumber = 0;
+            if (emptyBox[5] == 1)
             {
-                boxNumber = random.Next(0, 8);
+                boxNumber = 5;
+            }
+            else
+            {
+                while (emptyBox[boxNumber] > 1 && emptyBox[0] < 10)
+                {
+                    boxNumber = random.Next(1, 9);
+                }
+
             }
 
             switch (boxNumber)
             {
-                case (0):
+                case (1):
                     pictureBox1.Image = Image.FromFile(oPath);
                     emptyBox[boxNumber] = 3;
-                    emptyBox[9]++;
-                    break;
-                case (1):
-                    pictureBox2.Image = Image.FromFile(oPath);
-                    emptyBox[boxNumber] = 3;
-                    emptyBox[9]++;
+                    emptyBox[0]++;
                     break;
                 case (2):
-                    pictureBox3.Image = Image.FromFile(oPath);
+                    pictureBox2.Image = Image.FromFile(oPath);
                     emptyBox[boxNumber] = 3;
-                    emptyBox[9]++;
+                    emptyBox[0]++;
                     break;
                 case (3):
-                    pictureBox4.Image = Image.FromFile(oPath);
+                    pictureBox3.Image = Image.FromFile(oPath);
                     emptyBox[boxNumber] = 3;
-                    emptyBox[9]++;
+                    emptyBox[0]++;
                     break;
                 case (4):
-                    pictureBox5.Image = Image.FromFile(oPath);
+                    pictureBox4.Image = Image.FromFile(oPath);
                     emptyBox[boxNumber] = 3;
-                    emptyBox[9]++;
+                    emptyBox[0]++;
                     break;
                 case (5):
-                    pictureBox6.Image = Image.FromFile(oPath);
+                    pictureBox5.Image = Image.FromFile(oPath);
                     emptyBox[boxNumber] = 3;
-                    emptyBox[9]++;
+                    emptyBox[0]++;
                     break;
                 case (6):
-                    pictureBox7.Image = Image.FromFile(oPath);
+                    pictureBox6.Image = Image.FromFile(oPath);
                     emptyBox[boxNumber] = 3;
-                    emptyBox[9]++;
+                    emptyBox[0]++;
                     break;
                 case (7):
-                    pictureBox8.Image = Image.FromFile(oPath);
+                    pictureBox7.Image = Image.FromFile(oPath);
                     emptyBox[boxNumber] = 3;
-                    emptyBox[9]++;
+                    emptyBox[0]++;
                     break;
                 case (8):
+                    pictureBox8.Image = Image.FromFile(oPath);
+                    emptyBox[boxNumber] = 3;
+                    emptyBox[0]++;
+                    break;
+                case (9):
                     pictureBox9.Image = Image.FromFile(oPath);
                     emptyBox[boxNumber] = 3;
-                    emptyBox[9]++;
+                    emptyBox[0]++;
                     break;
                 default:
                     break;
             }
         }
 
-        public static short[] emptyBox = new short[10] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 2 };
-        // 1 - true, 2 - crosses, 3 - naughts, last number is for turn counter
+        public static int[] emptyBox = new int[10] { 2, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+        // 1 - true, 2 - crosses, 3 - naughts, first number is for turn counter
+        public static int[,] winningLane = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 1, 4, 6 }, { 2, 5, 7 }, { 3, 6, 9 }, { 1, 5, 8 }, { 3, 5, 6 } };
         public static string oPath = Path.Combine(System.IO.Directory.GetCurrentDirectory(), "O.png");
         public static string xPath = Path.Combine(System.IO.Directory.GetCurrentDirectory(), "X.png");
         //public static string oPath = ("http://cs616625.vk.me/v616625282/aff5/NQHFmJ6HgCc.jpg");
@@ -101,104 +110,128 @@ namespace WindowsForms
             pictureBox1.Width = image.Width;*/
         }
 
-
+        public int almostWin(int player) //2 - crosses, 3 - naughts
+        {
+            int total = 0;
+            for (int combination = 0; combination <= 7; combination++)
+            {
+                for (int positionToExclude = 0; positionToExclude <= 2; positionToExclude++)
+                {
+                    for (int position = 0; position <= 2; position++)
+                    {
+                        if (position != positionToExclude)
+                        {
+                            total = total + emptyBox[winningLane[combination, position]];
+                        }
+                        total = total + player;
+                        if (total == 6 || total ==9)
+                        {
+                            return combination;
+                            //emptyBox empty value is need to be "0" instead of "1" beceuae of 1+2+3 case
+                        }
+                        total = 0;
+                    }
+                }
+            }
+            return 10;
+        }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            if (emptyBox[0] == 1)
+            if (emptyBox[1] == 1)
             {
                 pictureBox1.Image = Image.FromFile(xPath);
-                emptyBox[0] = 2;
-                emptyBox[9]++;
-                AITrun();
+                emptyBox[1] = 2;
+                emptyBox[0]++;
+                AITurn();
             }
         }
 
         private void pictureBox2_Click(object sender, EventArgs e)
         {
-            if (emptyBox[1] == 1)
+            if (emptyBox[2] == 1)
             {
                 pictureBox2.Image = Image.FromFile(xPath);
-                emptyBox[1] = 2;
-                emptyBox[9]++;
-                AITrun();
+                emptyBox[2] = 2;
+                emptyBox[0]++;
+                AITurn();
             }
         }
 
         private void pictureBox3_Click(object sender, EventArgs e)
         {
-            if (emptyBox[2] == 1)
+            if (emptyBox[3] == 1)
             {
                 pictureBox3.Image = Image.FromFile(xPath);
-                emptyBox[2] = 2;
-                emptyBox[9]++;
-                AITrun();
+                emptyBox[3] = 2;
+                emptyBox[0]++;
+                AITurn();
             }
         }
 
         private void pictureBox4_Click(object sender, EventArgs e)
         {
-            if (emptyBox[3] == 1)
+            if (emptyBox[4] == 1)
             {
                 pictureBox4.Image = Image.FromFile(xPath);
-                emptyBox[3] = 2;
-                emptyBox[9]++;
-                AITrun();
+                emptyBox[4] = 2;
+                emptyBox[0]++;
+                AITurn();
             }
         }
 
         private void pictureBox5_Click(object sender, EventArgs e)
         {
-            if (emptyBox[4] == 1)
+            if (emptyBox[5] == 1)
             {
                 pictureBox5.Image = Image.FromFile(xPath);
-                emptyBox[4] = 2;
-                emptyBox[9]++;
-                AITrun();
+                emptyBox[5] = 2;
+                emptyBox[0]++;
+                AITurn();
             }
         }
 
         private void pictureBox6_Click(object sender, EventArgs e)
         {
-            if (emptyBox[5] == 1)
+            if (emptyBox[6] == 1)
             {
                 pictureBox6.Image = Image.FromFile(xPath);
-                emptyBox[5] = 2;
-                emptyBox[9]++;
-                AITrun();
+                emptyBox[6] = 2;
+                emptyBox[0]++;
+                AITurn();
             }
         }
 
         private void pictureBox7_Click(object sender, EventArgs e)
         {
-            if (emptyBox[6] == 1)
+            if (emptyBox[7] == 1)
             {
                 pictureBox7.Image = Image.FromFile(xPath);
-                emptyBox[6] = 2;
-                emptyBox[9]++;
-                AITrun();
+                emptyBox[7] = 2;
+                emptyBox[0]++;
+                AITurn();
             }
         }
 
         private void pictureBox8_Click(object sender, EventArgs e)
         {
-            if (emptyBox[7] == 1)
+            if (emptyBox[8] == 1)
             {
                 pictureBox8.Image = Image.FromFile(xPath);
-                emptyBox[7] = 2;
-                emptyBox[9]++;
-                AITrun();
+                emptyBox[8] = 2;
+                emptyBox[0]++;
+                AITurn();
             }
         }
 
         private void pictureBox9_Click(object sender, EventArgs e)
         {
-            if (emptyBox[8] == 1)
+            if (emptyBox[9] == 1)
             {
                 pictureBox9.Image = Image.FromFile(xPath);
-                emptyBox[8] = 2;
-                emptyBox[9]++;
-                AITrun();
+                emptyBox[9] = 2;
+                emptyBox[0]++;
+                AITurn();
             }
         }
     }
